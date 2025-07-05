ad1067a4ea8b3b5707d480593054fe7039f4a1ba
Shivpreet Saini <shivpreet362@gmail.com>
2025-07-05 21:02:51 +0530

    Refactor problem2.py to accept user input for numbers and update comments for clarity; enhance into_to_strings.py by adding print statements for string slicing examples.

--- chapter 2/problem2.py
+++ chapter 2/problem2.py
@@ -1,6 +1,6 @@
 # Write a Python progran to find remainder when a number is divided by z
-a=12
+a=int(input("enter a number: "))
 
-b=5
+b=int(input("enter another number: "))
 
-print(a % b)  # This will print the remainder when 12 is divided by 5
+print(a % b)  # This will print the remainder when a is divided by b
--- chapter 3/into_to_strings.py
+++ chapter 3/into_to_strings.py
@@ -5,3 +5,6 @@ print(short_name)
 
 character1= name[1]
 print(character1)
+
+print(name[:4]) # its same as name[0:4]
+print(name[1:]) # its same as name[1:0]
